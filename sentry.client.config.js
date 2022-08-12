210ef6bfee5d0e393832a90f3d6249901800a0b3
Dien Vu <meotimdihia@gmail.com>
2022-08-13 00:27:45 +0700

    Update sentry.client.config.js

--- sentry.client.config.js
+++ sentry.client.config.js
@@ -7,7 +7,7 @@ import * as Sentry from '@sentry/nextjs'
 const SENTRY_DSN = process.env.SENTRY_DSN || process.env.NEXT_PUBLIC_SENTRY_DSN
 
 Sentry.init({
-  dsn: SENTRY_DSN,
+  dsn: SENTRY_DSN || "https://149848e38b354c9c84abc2def3ea0902@sntr.comick.fun/3",,
   tracesSampleRate: 1.0,
   // Note: if you want to override the automatic release value, do not set a
   // `release` value here - use the environment variable `SENTRY_RELEASE`, so
