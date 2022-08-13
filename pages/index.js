25b380ff4f75d8ef1f4ed94fb1a84ccf1f48ce7e
Dien Vu <meotimdihia@gmail.com>
2022-08-13 14:50:30 +0700

    -

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,14 @@
 import Link from 'next/link'
-import {useEffect} from 'react'
+import {useState, useEffect} from 'react'
 
 export default function Index () {
+	const [show, setShow] = useState(false)
 	useEffect(() => {
-		alert('test')
+		setShow(true)
 	})
 	return (
   <div style={{ maxWidth: 700, margin: '0 auto' }}>
+		<h1>{show ? "Everything is fine" : "There is a bug"}</h1>
     <h2>Sentry Simple Example 🚨</h2>
     <p>
       This example demonstrates how to record unhandled exceptions in your code
