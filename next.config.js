d5e028d69bb62cc382523d031358182872b0cbbd
Damien Burke <damien@damienburke.com>
2022-08-31 14:35:59 -0700

    Give a non-blank release value

--- next.config.js
+++ next.config.js
@@ -20,7 +20,7 @@ const sentryWebpackPluginOptions = {
   // For all available options, see:
   // https://github.com/getsentry/sentry-webpack-plugin#options.
 
-  release: '', // override release value
+  release: 'DEADBEEF', // override release value
 };
 
 // Make sure adding Sentry options is the last code to run before exporting, to
